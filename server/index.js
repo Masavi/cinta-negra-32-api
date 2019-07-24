374c70e6af85c732935fe5bd65def8c3f679ac88
maui <maui@devf.mx>
2019-07-23 20:54:41 -0500

    agregar endpoints de crear usuario y obtener usuarios

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+const User = require('../models/User');
  
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,11 +13,17 @@ app.get('/', (req, res) => {
     res.status(200).send('¡Hola Mundo!');
 });
 
+app.post('/api/v1/user', (req, res) => {
+    const newUser = new User(req.body);
+          newUser.save()
+                 .then( newUser => res.status(201).send(newUser))
+                 .catch( err => res.status(400).send(err));
+});
+
 app.get('/api/v1/user', (req, res) => {
-    res.status(200).send({
-        "message": "ok",
-        "users": ["Juan", "Paco", "Luis"],
-    })
+    User.find()
+        .then( users => res.status(200).send(users))
+        .catch( err => res.status(400).send(err));
 });
 
 module.exports = { app, PORT };
